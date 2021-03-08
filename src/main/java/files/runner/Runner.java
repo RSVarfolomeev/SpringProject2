5e4eab34e97e59c5bac8a6b2d9049ef6fa1bf30d
VAR-VAR <hrom9090@mail.ru>
2021-03-08 15:48:20 +0300

    Выполнение 1, 2 задания.

---
+++ src/main/java/files/runner/Runner.java
@@ -0,0 +1,36 @@
+//package files.runner;
+//
+//
+//import files.print.Print;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Runner implements CommandLineRunner {
+//
+//    @Autowired
+//    private Print printToConsole;
+////    private final Print printToConsole;
+////    private final Print printToFile;
+////    private final List<String> names;
+////
+////    public Runner(Print printToConsole,
+////                  Print printToFile,
+////                  List<String> names) {
+////        this.printToConsole = printToConsole;
+////        this.printToFile = printToFile;
+////        this.names = names;
+////    }
+//
+////    @PostConstruct
+////    private void postConstruct() {
+////        System.out.println("postCont");
+////    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        printToConsole.print("Тест");
+////        printToFile.print("Тест файл");
+//    }
+//}
