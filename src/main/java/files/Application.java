fce7d2900d012f38479c2fc77fc3b7246e65d363
VAR-VAR <hrom9090@mail.ru>
2021-03-14 19:59:01 +0300

    Выполнение 1, 2, 3 задания.

--- src/main/java/files/Application.java
+++ src/main/java/files/Application.java
@@ -7,22 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        ApplicationContext context = new AnnotationConfigApplicationContext(ProductRepository.class, Cart.class);
-        ProductRepository productList = context.getBean(ProductRepository.class);
-        System.out.println(productList.getProductList());
-        System.out.println(productList.getProductListById(2));
-        Cart cart = context.getBean(Cart.class);
-        cart.addProduct(1);
-        cart.addProduct(2);
-        cart.addProduct(3);
-        System.out.println(cart.getCartList());
-        cart.deleteCartListById(2);
-        System.out.println(cart.getCartList());
     }
 
 }
